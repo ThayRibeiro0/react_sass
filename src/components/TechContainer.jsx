3407d5e8b2459f924e2d97e8f333790e6d17a81c
Thays Ribeiro <94051605+ThayRibeiro0@users.noreply.github.com>
2023-10-12 22:38:07 -0700

    9

--- src/components/TechContainer.jsx
+++ src/components/TechContainer.jsx
@@ -1,8 +1,42 @@
-import React from 'react'
+import {
+  DiHtml5,
+  DiCss3,
+  DiJsBadge,
+  DiNodejsSmall,
+  DiMysql,
+  DiReact
+} from 'react-icons/di'
+
+import '../styles/components/techcontainer.sass'
+
+const tech = [
+  { id: "html", name: "HTML5", icon: <DiHtml5 /> },
+  { id: "css", name: "CSS3", icon: <DiCss3 /> },
+  { id: "js", name: "JavaScript", icon: <DiJsBadge /> },
+  { id: "node", name: "Node.js", icon: <DiNodejsSmall /> },
+  { id: "mysql", name: "MySQL", icon: <DiMysql /> },
+  { id: "react", name: "React", icon: <DiReact /> },
+]
 
 const TechContainer = () => {
   return (
-    <div>TechContainer</div>
+    <section className="tech-container">
+      <h2>Technologies</h2>
+      <div className="tech-grid">
+        {tech.map((technologies) => (
+          <div className="tech-card" id={technologies.id} key={technologies.id}>
+            {technologies.icon}
+            <div className="tech-info">
+              <h3>{technologies.name}</h3>
+              <p>
+                what a know about it
+              </p>
+            </div>
+
+          </div>
+        ))}
+      </div>
+    </section>
   )
 }
 
